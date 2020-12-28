f4798008bccd24edfebb5a703ed9a4252c86c812
chaomonica <alittlefaun@gmail.com>
2020-12-28 11:18:26 -0600

    began work on react router

--- front-end/src/components/MainPage/MainPage.js
+++ front-end/src/components/MainPage/MainPage.js
@@ -1,15 +1,36 @@
 import React, {useState} from 'react';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/core/Menu';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+
+import { Link, Route, Switch } from 'react-router-dom';
+
 // eslint-disable-next-line func-style
 const MainPage = () => {
 
   return (
     <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" >
+      ICON HERE
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <div>
         <h1>HELLO TEAM TRUFFLE</h1>
       </div>
     </>
   );
-}
+};
 
 export default MainPage;
