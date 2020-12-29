26be8c078320f42a679b03d8a93392a53f82f37b
Kornelija Laucius <kornelija.janusaite@gmail.com>
2020-12-29 11:18:31 -0600

    cp

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
