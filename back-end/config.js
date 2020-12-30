81a7faf1322d4b2868c71b19fb6b3806e03818bc
Rick Hawley <rickhawley109@gmail.com>
2020-12-30 16:22:28 -0600

    database setup! co-author: lucy luceestrong

--- back-end/config.js
+++ back-end/config.js
@@ -4,5 +4,5 @@ module.exports = {
   MONGO_USER: process.env.MONGO_USERNAME,
   MONGO_PASS: process.env.MONGO_PASSWORD,
   MONGO_URI: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@hungry.jrmv9.mongodb.net/hungry?retryWrites=true&w=majority`,
-  EC2_URI: '',
+  EC2_URI: 'mongodb://hungrybunch:eateateat@3.138.189.191/HUNGRYBUNCH?authSource=admin',
 };
