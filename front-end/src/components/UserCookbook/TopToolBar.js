677c57fc1618fd0b949e8348882a2c65938a9382
chaomonica <alittlefaun@gmail.com>
2020-12-30 14:30:34 -0600

    added dynamically rendering nickname to cookbook header and static avatar to header

--- front-end/src/components/UserCookbook/TopToolBar.js
+++ front-end/src/components/UserCookbook/TopToolBar.js
@@ -8,43 +8,49 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link, Route, Switch } from 'react-router-dom';
 
+const useStyles = makeStyles((theme)=>({
+  centerText: {
+    flexGrow: 1,
+    textAlign: 'center',
+  },
+  avatar: {
+    width: theme.spacing(7),
+    height: theme.spacing(7)
+  }
+}));
 
-const TopToolbar = () => {
+const TopToolbar = (props) => {
+  const classes = useStyles();
 
   return (
     <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
 
-          </IconButton>
-          <Link to="/">
-            <img src="https://i.pinimg.com/originals/60/ed/ab/60edabe557e8139d52dae12f380205dc.png" width="100" height="100" ></img>
-          </Link>
-
-
-
-          <Typography variant="h6" className={classes.title}>
-            HUNGRY BUNCH
-          </Typography>
+        </IconButton>
+        <Link to="/">
+          <img src="https://i.pinimg.com/originals/60/ed/ab/60edabe557e8139d52dae12f380205dc.png" width="100" height="100" ></img>
+        </Link>
 
 
-          <Button color="inherit">
-            <Link to="/login">Login</Link>
-          </Button>
-          <Button color="inherit">
-            <Link to="/signup">Sign up</Link>
-          </Button>
-          <Button color="inherit">
-            <Link to="/">Sign out</Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
+        <Avatar className={classes.avatar}>H</Avatar>
+        <Typography variant="h2" className={classes.centerText}>
+          {props.nickname}
+        </Typography>
 
-  )
 
-}
+        <Button color="inherit">
+          <Link to="/">Sign out</Link>
+        </Button>
+      </Toolbar>
+    </AppBar>
+
+  );
+
+};
 
 export default TopToolbar;
--- front-end/src/components/UserCookbook/UserCookbook.js
+++ front-end/src/components/UserCookbook/UserCookbook.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Link, Route, Switch } from 'react-router-dom';
 
 import TopToolbar from './TopToolBar.js';
+import { useAuth0 } from '@auth0/auth0-react';
 
 //wireframe: https://www.figma.com/file/C9TLcX8c0DNBW3xsYlv6kO/Untitled?node-id=60%3A2
 /*
@@ -23,12 +24,21 @@ const useStyles = makeStyles(theme => ({
 const UserCookbook = () => {
 
   const classes = useStyles();
-  return (
-    <>
-      <TopToolbar />
+  const { user } = useAuth0();
 
-    </>
-  );
+  if (user) {
+    const { name, picture, email, nickname } = user;
+
+    return (
+      <>
+        <TopToolbar nickname={nickname} picture={picture}/>
+        <p>ghello, am cookbook</p>
+        {console.warn('this is warning', user)}
+      </>
+    );
+  } else {
+    return (<p>Eep! You shouldn't see this!</p>);
+  }
 };
 
 export default UserCookbook;
