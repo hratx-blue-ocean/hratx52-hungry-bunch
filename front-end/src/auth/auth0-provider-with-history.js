6e29fa3368e4087a752f601151d010b83b1cdf29
chaomonica <alittlefaun@gmail.com>
2020-12-30 11:13:49 -0600

    made changes to webpack var/const and /auth /login

--- front-end/src/auth/auth0-provider-with-history.js
+++ front-end/src/auth/auth0-provider-with-history.js
@@ -1,6 +1,6 @@
-// import React from "react";
-// import { useHistory } from "react-router-dom";
-// import { Auth0Provider } from "@auth0/auth0-react";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const Auth0ProviderWithHistory = ({ children }) => {
   const domain = 'dev-c0ooqztb.us.auth0.com';
@@ -9,7 +9,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const history = useHistory();
 
   const onRedirectCallback = (appState) => {
-    //history.push(appState?.returnTo || window.location.pathname);
+    history.push(appState?.returnTo || window.location.pathname);
   };
 
   return (
