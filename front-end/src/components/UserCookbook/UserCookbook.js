df19f41a36e85eb08a00f196583312815624f37f
chaomonica <alittlefaun@gmail.com>
2020-12-30 12:15:44 -0600

    continued setting up components before pull from development

--- front-end/src/components/UserCookbook/UserCookbook.js
+++ front-end/src/components/UserCookbook/UserCookbook.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link, Route, Switch } from 'react-router-dom';
 
+import TopToolbar from './TopToolBar.js';
 
 //wireframe: https://www.figma.com/file/C9TLcX8c0DNBW3xsYlv6kO/Untitled?node-id=60%3A2
 /*
@@ -24,6 +25,7 @@ const UserCookbook = () => {
   const classes = useStyles();
   return (
     <>
+      <TopToolbar />
 
     </>
   );
