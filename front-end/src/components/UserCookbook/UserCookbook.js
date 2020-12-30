75523ba87032e052ac3922cb0a5adef74a3b7374
chaomonica <alittlefaun@gmail.com>
2020-12-30 11:56:03 -0600

    began set up of component skeleton for usercookbook

---
+++ front-end/src/components/UserCookbook/UserCookbook.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link, Route, Switch } from 'react-router-dom';
+
+
+//wireframe: https://www.figma.com/file/C9TLcX8c0DNBW3xsYlv6kO/Untitled?node-id=60%3A2
+/*
+top toolbar
+category carousel
+recipe tiles
+friend toolbar on left?
+*/
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+  },
+}));
+
+
+const UserCookbook = () => {
+
+  const classes = useStyles();
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default UserCookbook;
