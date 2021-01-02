86eac9c53468a053b6a6595b0135bb0658cce899
Kela Kealakai <k12kekeal@gmail.com>
2021-01-02 13:40:48 -0600

    Pulling front development didn't update this properly on my local side. Fixed!

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -4,16 +4,12 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <>
-      <h1>Click here to sign in</h1>
-      <button onClick={() => dispatch(setUser('testUser'))}>Sign in</button>
-      <button
-        className="btn btn-primary btn-block"
-        onClick={() => loginWithRedirect()}
-      >
-        Log In
-      </button>
-    </>
+    <button
+      className="btn btn-primary btn-block"
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
   );
 };
 
