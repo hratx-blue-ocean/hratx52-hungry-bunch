e0d59c842f72fc750f4f3fd3cbdbdf997632a0b1
chaomonica <alittlefaun@gmail.com>
2021-01-02 11:01:16 -0600

    pulled from development and added a new comment

--- front-end/src/spec/Cookbook.spec.js
+++ front-end/src/spec/Cookbook.spec.js
@@ -6,7 +6,7 @@ configure({ adapter: new Adapter() });
 
 /*
 provides a React renderer that can be used to render React components to pure JavaScript objects,
-without depending on the DOM or a native mobile environment
+without depending on the DOM or a native mobile environment..
 */
 import renderer from 'react-test-renderer';
 
