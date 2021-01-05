c6ac6c4afd83f98a5fa1b3cc6e83e7cea4ad3743
chaomonica <alittlefaun@gmail.com>
2021-01-05 11:09:30 -0600

    edited individualFriend to match with Kela's avatar link in future

--- front-end/src/components/RightToolBar/IndividualFriend.js
+++ front-end/src/components/RightToolBar/IndividualFriend.js
@@ -8,16 +8,22 @@ import User from '../User/User.js';
 
 
 const IndividualFriend = ({ singleFriend }) => {
+  //userId for a friend will be in the original user's object received from the GET request
+  //the User component will then use the url to make its own axios request
+
+  let userId = 1232132;
+  let friendLink = `./user/${userId}`;
+
   return (
     <Grid container item spacing={2}>
 
       <Grid item>
-        <a href='/user'>
+        <a href= {friendLink}>
           <Avatar src={singleFriend.picture}></Avatar>
         </a>
       </Grid>
       <Grid item>
-        <a href='/user'>
+        <a href= {friendLink}>
           {singleFriend.name}
         </a>
       </Grid>
