3d6e680e9f1b7662d493838dbf510a1a88640373
Jonathan Garcia <jagspurs7@gmail.com>
2021-01-06 11:48:07 -0600

    connected center console to the store to get the user object. Also connected right tool bar, so i can pass the user object friends array to the friends list. co-authored: Monica <chamonica>

--- front-end/src/components/RightToolBar/IndividualFriend.js
+++ front-end/src/components/RightToolBar/IndividualFriend.js
@@ -1,29 +1,28 @@
 import React, { useState } from 'react';
 import { Grid, Container, Paper, IconButton } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
+import {Link} from 'react-router-dom';
 
-const IndividualFriend = ({ singleFriend }) => {
+const IndividualFriend = ({ singleFriend, friend }) => {
   //userId for a friend will be in the original user's object received from the GET request
   //the User component will then use the url to make its own axios request
 
-  let userId = 1232132;
-  let friendLink = `./user/${userId}`;
+  let userId = '5ff2381f11fa42c5cd024b55';
+  let friendLink = `/user/${userId}`;
 
   return (
     <Grid container item spacing={2}>
-
       <Grid item>
-        <a href= {friendLink}>
+        <Link to={friendLink}>
           <Avatar src={singleFriend.picture}></Avatar>
-        </a>
+        </Link>
       </Grid>
       <Grid item>
-        <a href= {friendLink}>
+        <Link to={friendLink}>
           {singleFriend.name}
-        </a>
+        </Link>
       </Grid>
     </Grid>
-
   );
 };
 
