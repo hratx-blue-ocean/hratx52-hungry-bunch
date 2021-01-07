e8306770e0a2fc28bb0902bdd9e444b92f696a18
chaomonica <alittlefaun@gmail.com>
2021-01-07 11:03:06 -0600

    fixed bug in which individual user page was not updating from redirect in friendslist on the Users page

--- front-end/src/components/User/User.js
+++ front-end/src/components/User/User.js
@@ -2,57 +2,25 @@ import React, {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 
-import { useAuth0 } from '@auth0/auth0-react';
 
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Container } from '@material-ui/core';
-
-import TopToolbar from './TopToolBar.js';
-import SetFilterCategory from './SetFilterCategory.js';
-import CategoryCarousel from './CategoryCarousel.js';
-import CenterConsole from '../CenterConsole/CenterConsole.js';
-import { default as LeftToolbar } from '../RightToolBar/RightToolBar.js';
-import UserDbCalls from './UserDbCall.js'
-
-
+import UserDbCalls from './UserDbCall.js';
 //wireframe: https://www.figma.com/file/C9TLcX8c0DNBW3xsYlv6kO/Untitled?node-id=60%3A2
-
-const useStyles = makeStyles(theme => ({
-  title: {
-    flexGrow: 1,
-    textAlign: 'center',
-  },
-}));
 
 
 const User = () => {
-
-  const classes = useStyles();
-  const { user } = useAuth0();
-  const [currCategory, setCurrCategory] = useState('');
-  const state = useSelector(state => state);
 
   //React router to grab pathname
   const location = useLocation();
   const history = useHistory();
 
-  if (user) {
+  /*can either use: location.pathname OR history.location.pathname*/
+  let userId = location.pathname.split('/')[2];
 
-    const { name, picture, email, nickname } = user;
-
-    /*can either use: location.pathname OR history.location.pathname*/
-    let userId = location.pathname.split('/')[2];
-
-
-
-    return (
-      <>
-        <UserDbCalls location={location}/>
-      </>
-    );
-  } else {
-    return (<></>);
-  }
+  return (
+    <>
+      <UserDbCalls location={location.pathname.split('/')[2]}/>
+    </>
+  );
 };
 
 export default User;
--- front-end/src/components/User/UserDbCall.js
+++ front-end/src/components/User/UserDbCall.js
@@ -9,55 +9,57 @@ import { default as LeftToolbar } from '../RightToolBar/RightToolBar.js';
 import axios from 'axios';
 
 class UserBbCalls extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            urlUserInfo: {},
-            urlUserId: '5ff4903962127775787d7d8f'
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      urlUserInfo: {},
+      urlUserId: '5ff4903962127775787d7d8f'
+    };
+    this.dbCallUserInfo = this.dbCallUserInfo.bind(this);
+  }
 
-    componentDidMount() {
-        let userId = window.location.pathname.split('/')[2];
-        axios.get(`http://localhost:3000/userInfo/${userId}`)
-            .then((userUrlInfoDb) => {
-                this.setState({
-                    urlUserId: userId,
-                    urlUserInfo: userUrlInfoDb.data
-                });
-            })
-            .catch((error) => { console.log('error in axios request in setFilterCategory: ', error); });
-    }
+  dbCallUserInfo (userId) {
+    axios.get(`http://localhost:3000/userInfo/${userId}`)
+      .then((userUrlInfoDb) => {
+        this.setState({
+          urlUserId: userId,
+          urlUserInfo: userUrlInfoDb.data
+        });
+      })
+      .catch((error) => { console.log('error in axios request in setFilterCategory: ', error); });
+  }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        // let currentUrlId = window.location.pathname.split('/')[2]
-        // if (prevState.urlUserId !== currentUrlId) {
-        //   this.setState({urlUserId: currentUrlId})
-        // }
+  componentDidMount() {
+    let userId = this.props.location;
+    this.dbCallUserInfo(userId);
+  }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.location !== this.props.location) {
+      this.dbCallUserInfo(this.props.location);
     }
+  }
 
-    render() {
-        return (
-            <>
-                <TopToolbar nickname={this.state.urlUserInfo.name} picture={this.state.urlUserInfo.picture} />
-                {console.log('MONICA', this.props)}
-                <br></br>
-                <div>
-                    <Container>
-                        <Grid container>
-                            <Grid item xs={3}>
-                                {/*THIS IS THE FRIENDLIST*/}
-                                <LeftToolbar />
-                            </Grid>
-                            <Grid item xs={9}>
-                                <SetFilterCategory userId={this.state.urlUserId} userInfo={this.state.urlUserInfo} />
-                            </Grid>
-                        </Grid>
-                    </Container>
-                </div>
-            </>);
-    }
+  render() {
+    return (
+      <>
+        <TopToolbar nickname={this.state.urlUserInfo.name} picture={this.state.urlUserInfo.picture} />
+        <br></br>
+        <div>
+          <Container>
+            <Grid container>
+              <Grid item xs={3}>
+                {/*THIS IS THE FRIENDLIST*/}
+                <LeftToolbar />
+              </Grid>
+              <Grid item xs={9}>
+                <SetFilterCategory userId={this.state.urlUserId} userInfo={this.state.urlUserInfo} />
+              </Grid>
+            </Grid>
+          </Container>
+        </div>
+      </>);
+  }
 }
 
 export default UserBbCalls;
