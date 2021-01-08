c5c81f28e66089de977786359df1afe27ebc8905
chaomonica <alittlefaun@gmail.com>
2021-01-08 16:20:43 -0600

    deleted huge comment used during development

--- front-end/src/components/Recipe/RecipeData.jsx
+++ front-end/src/components/Recipe/RecipeData.jsx
@@ -4,9 +4,6 @@ import axios from 'axios';
 
 import { useHistory, useLocation } from 'react-router-dom';
 
-//delete later
-import {recipe} from '../../data/recipeDummyData.js';
-
 const RecipeData = (props) => {
   const user = `/user/${recipe.ownerId}`;
 
@@ -49,42 +46,7 @@ const RecipeData = (props) => {
   return (<></>);
 };
 
-// recipe name, ingredients, time/vegan/difficulty, steps
-
 export default RecipeData;
 
-// TODO: styling, use ownerId to get username, figure out how to use router to make username a link to that user's profile
 
 
-
-/*
-
-
-      <div className='recipe-data'>
-        <div className='recipe-name-and-user'>
-          <h1>{recipe.recipeName}</h1>
-          <Link to={`/user/${props.currRecipe.owner}`}>
-            <div>{props.currRecipe.owner} - username goes here!</div>
-          </Link>
-        </div>
-        <div className='recipe-ingredients'>
-          <ul>{props.currRecipe.ingredients.map((ingredient, i) => (
-            <li key={i}>{ingredient}</li>
-          ))}</ul>
-        </div>
-        <div className='optional-recipe-data'>
-          Category: {props.currRecipe.category}
-
-          Time: {props.currRecipe.time} minutes,
-
-          Difficulty: {props.currRecipe.difficulty}
-          {props.currRecipe.vegan ? ', Vegan' : null}
-        </div>
-        <div className='recipe-steps'>
-          <ol>{props.currRecipe.steps.map((step, i) => (
-            <li key={i}>{step}</li>
-          ))}</ol>
-        </div>
-      </div>
-
-*/
