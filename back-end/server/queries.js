d2564e06c407723c6e4a3de25da390944d891158
Rick Hawley <rickhawley109@gmail.com>
2021-01-08 10:28:13 -0600

    Get a recipe by the recipeID

--- back-end/server/queries.js
+++ back-end/server/queries.js
@@ -23,6 +23,17 @@ const GetUser = ({ id }, callback) => {
   });
 };
 
+const GetRecipe = ({ id }, callback) => {
+  Recipe.findById( id, (err, recipe) => {
+    if (err) {
+      log(chalk.redBright('err', err));
+      callback(err);
+    } else {
+      callback(null, recipe);
+    }
+  });
+};
+
 const GetFriends = ({ name }, callback) => {
   User.find({ 'name': { '$regex': name, '$options': 'i' }}, (err, friends) => {
     if (err) {
@@ -200,5 +211,6 @@ module.exports = {
   GetFriends,
   GetUserRecipes,
   GetAllRecipes,
-  UpdateFavoritedBy
+  UpdateFavoritedBy,
+  GetRecipe
 };
