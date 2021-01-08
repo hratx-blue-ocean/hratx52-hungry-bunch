022e4f95bd06b36bb67e43a7ca410e8e0790ddd1
chaomonica <alittlefaun@gmail.com>
2021-01-08 11:33:36 -0600

    implemented grabbing recipe data and passing data to the RecipeData and RecipeImage components from Recipe

--- front-end/src/components/Recipe/Recipe.jsx
+++ front-end/src/components/Recipe/Recipe.jsx
@@ -1,27 +1,41 @@
 
-import RecipeData from './RecipeData.jsx';
-import React from 'react';
-import {recipe} from '../../data/recipeDummyData.js';
-import './recipe.css';
-import RecipeImage from './RecipeImage.jsx';
-import Paper from '@material-ui/core/Paper';
-
+import React, { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
+
+import './recipe.css';
+
+import RecipeData from './RecipeData.jsx';
+import RecipeImage from './RecipeImage.jsx';
 
 export default function Recipe() {
   const location = useLocation();
   let recipeId = location.pathname.split('/')[2];
 
-  return (
-    <Paper
-      variant='outlined'>
-      <div className='recipe-container'>
-        <div className='recipe-page-container'>
-          <RecipeData recipeId={recipeId}/>
-        </div>
-        <RecipeImage />
-      </div>
-    </Paper>
+  const [currRecipe, setCurrRecipe] = useState({});
+  const [loading, setLoading] = useState({});
 
-  );
+  useEffect(() => {
+    axios.get(`http://localhost:3000/recipe/${recipeId}`)
+      .then((recipeDataInDb)=>{
+        setCurrRecipe(recipeDataInDb.data);
+      })
+      .catch((error) => { console.log('Axios ERROR in Recipe: ', error); });
+  }, []);
+
+  if (currRecipe.owner) {
+    return (
+      <Paper
+        variant='outlined'>
+        <div className='recipe-container'>
+          <div className='recipe-page-container'>
+            <RecipeData recipeData={currRecipe}/>
+          </div>
+          <RecipeImage image={currRecipe.imageUrl}/>
+        </div>
+      </Paper>
+    );
+  }
+  return (<></>);
 }
--- front-end/src/components/Recipe/RecipeData.jsx
+++ front-end/src/components/Recipe/RecipeData.jsx
@@ -2,47 +2,45 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
+import { useHistory, useLocation } from 'react-router-dom';
+
 //delete later
 import {recipe} from '../../data/recipeDummyData.js';
 
 const RecipeData = (props) => {
-
-  //delete later
   const user = `/user/${recipe.ownerId}`;
 
-  const [currRecipe] = useState({});
-  useEffect(() => {
+  if (props.recipeData && props.recipeData.owner) {
+    return (
+      <div className='recipe-data'>
+        <div className='recipe-name-and-user'>
+          <h1>{recipe.recipeName}</h1>
+          <Link to={`/user/${props.recipeData.owner}`}>
+            <div>{props.recipeData.owner} - username goes here!</div>
+          </Link>
+        </div>
+        <div className='recipe-ingredients'>
+          <ul>{props.recipeData.ingredients.map((ingredient, i) => (
+            <li key={i}>{ingredient}</li>
+          ))}</ul>
+        </div>
+        <div className='optional-recipe-data'>
+          Category: {props.recipeData.category}
 
-  });
+          Time: {props.recipeData.time} minutes,
 
-  return (
-    <div className='recipe-data'>
-      <div className='recipe-name-and-user'>
-        <h1>{recipe.recipeName}</h1>
-        <Link to={user}>
-          <div>{recipe.ownerId} - username goes here!</div>
-        </Link>
+          Difficulty: {props.recipeData.difficulty}
+          {props.recipeData.vegan ? ', Vegan' : null}
+        </div>
+        <div className='recipe-steps'>
+          <ol>{props.recipeData.steps.map((step, i) => (
+            <li key={i}>{step}</li>
+          ))}</ol>
+        </div>
       </div>
-      <div className='recipe-ingredients'>
-        <ul>{recipe.ingredients.map((ingredient, i) => (
-          <li key={i}>{ingredient}</li>
-        ))}</ul>
-      </div>
-      <div className='optional-recipe-data'>
-        Category: {recipe.category}
-
-        Time: {recipe.time} minutes,
-
-        Difficulty: {recipe.difficulty}
-        {recipe.vegan ? ', Vegan' : null}
-      </div>
-      <div className='recipe-steps'>
-        <ol>{recipe.steps.map((step, i) => (
-          <li key={i}>{step}</li>
-        ))}</ol>
-      </div>
-    </div>
-  );
+    );
+  }
+  return (<></>);
 };
 
 // recipe name, ingredients, time/vegan/difficulty, steps
@@ -50,3 +48,37 @@ const RecipeData = (props) => {
 export default RecipeData;
 
 // TODO: styling, use ownerId to get username, figure out how to use router to make username a link to that user's profile
+
+
+
+/*
+
+
+      <div className='recipe-data'>
+        <div className='recipe-name-and-user'>
+          <h1>{recipe.recipeName}</h1>
+          <Link to={`/user/${props.currRecipe.owner}`}>
+            <div>{props.currRecipe.owner} - username goes here!</div>
+          </Link>
+        </div>
+        <div className='recipe-ingredients'>
+          <ul>{props.currRecipe.ingredients.map((ingredient, i) => (
+            <li key={i}>{ingredient}</li>
+          ))}</ul>
+        </div>
+        <div className='optional-recipe-data'>
+          Category: {props.currRecipe.category}
+
+          Time: {props.currRecipe.time} minutes,
+
+          Difficulty: {props.currRecipe.difficulty}
+          {props.currRecipe.vegan ? ', Vegan' : null}
+        </div>
+        <div className='recipe-steps'>
+          <ol>{props.currRecipe.steps.map((step, i) => (
+            <li key={i}>{step}</li>
+          ))}</ol>
+        </div>
+      </div>
+
+*/
